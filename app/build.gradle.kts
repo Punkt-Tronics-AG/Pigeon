b3d31e1aa29e59b6f09b829f52b81d2990118b23
Mateusz Kata <mkata@mobitouch.net>
2025-09-12 21:17:22 +0200

    update ringrtc library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -501,7 +501,7 @@ dependencies {
   coreLibraryDesugaring(libs.android.tools.desugar)
 
   // for Pigeon
-  implementation(files("libs/ringrtc-android-2.56.0.aar"))
+  implementation(files("libs/ringrtc-android-2.57.0.aar"))
 
 
   implementation(project(":libsignal-service"))
