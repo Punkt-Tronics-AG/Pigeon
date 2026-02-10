209791d6776313947c050504438c7eb560d2a265
Mateusz Kata <mkata@mobitouch.net>
2026-02-10 22:13:04 +0100

    remove old unused views

---
+++ core/ui/src/main/java/pigeon/extensions/CoreBuildExtensions.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2026 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package pigeon.extensions
+
+fun isSignalVersion(): Boolean = false
+
+fun isPigeonVersion(): Boolean = true
+
